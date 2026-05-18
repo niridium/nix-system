bf9245c929114cbc15dd64a007710430cff8cee4
niridium <niridium@tuta.io>
2026-05-18 21:24:41 +0200

    ops: add pre-commit hooks

--- devenv.nix
+++ devenv.nix
@@ -1,13 +1,23 @@
 {pkgs, ...}: {
   packages = with pkgs; [
     git
-    # git-conventional-commits
-    # pre-commit
+    git-conventional-commits
   ];
 
   languages.nix.enable = true;
 
-  git-hooks.hooks = {
-    alejandra.enable = true;
+  git-hooks = {
+    hooks = {
+      alejandra.enable = true;
+      deadnix.enable = true;
+
+      git-conventional-commits = {
+        enable = true;
+        name = "git-conventional-commits";
+        entry = "git-conventional-commits commit-msg-hook";
+        stages = ["commit-msg"];
+        language = "node";
+      };
+    };
   };
 }
