34322dea55b40c8c60a45bf6df98ba10d4b0bb49
niridium <niridium@tuta.io>
2026-06-20 00:19:39 +0200

    feat(gtk): follow noctalia theme with custom font and papirus icons

--- modules/gui/gtk.nix
+++ modules/gui/gtk.nix
@@ -1,10 +1,24 @@
 {
   flake.modules.homeManager.gui = {pkgs, ...}: {
+    # home.packages = [pkgs.nwg-look];
     gtk = {
       enable = true;
+      gtk4.theme = null;
+      theme = {
+        package = pkgs.adw-gtk3;
+        name = "adw-gtk3";
+      };
+      font = {
+        name = "Iosevka Nerd Font";
+        size = 11;
+      };
+      # cursorTheme = {
+      #   package = pkgs.catppuccin-cursors.mochaDark;
+      #   name = "catpuccin-mocha-dark-cursors";
+      # };
       iconTheme = {
-        package = pkgs.adwaita-icon-theme;
-        name = "Adwaita";
+        package = pkgs.papirus-icon-theme;
+        name = "Papirus";
       };
     };
   };
