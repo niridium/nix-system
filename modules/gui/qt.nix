4ee3edc41d9ae2651546db5ac4e894f1cc83205b
niridium <niridium@tuta.io>
2026-06-19 19:24:41 +0200

    feat: use breeze style with noctalia theme and custom font

--- modules/gui/qt.nix
+++ modules/gui/qt.nix
@@ -1,9 +1,21 @@
 {
-  flake.modules.homeManager.gui = {
+  flake.modules.homeManager.gui = {pkgs, ...}: {
+    home.packages = [pkgs.kdePackages.qt6ct];
     qt = {
       enable = true;
-      platformTheme.name = "adwaita";
-      style.name = "adwaita";
+      platformTheme.name = "qt6ct";
+      qt6ctSettings = {
+        Appearance = {
+          color_scheme_path = "/home/nixy/.config/qt6ct/colors/noctalia.conf";
+          custom_palette = true;
+          style = "Breeze";
+        };
+        Fonts = {
+          fixed = "\"Iosevka Nerd Font Mono,12\"";
+          general = "\"Iosevka Nerd Font,12\"";
+        };
+      };
+      style.name = "breeze";
     };
   };
 }
