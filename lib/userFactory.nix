8922d5d60bb8cbebcdede669a6446f3af7140f95
niridium <niridium@tuta.io>
2026-08-04 19:54:06 +0200

    refactor: make a better use of the "user" argument

--- lib/userFactory.nix
+++ lib/userFactory.nix
@@ -1,6 +1,6 @@
 {
-  config,
   homeManager,
+  config,
   user,
   lib,
   ...
@@ -8,14 +8,12 @@
   options.glaciux.users.${user}.enable = lib.mkEnableOption "${user} user";
   config = lib.mkIf config.glaciux.users.${user}.enable {
     home-manager.users.${user} = homeManager;
-    users.users = builtins.listToAttrs (map (user:
-      lib.nameValuePair user {
-        isNormalUser = true;
-        extraGroups =
-          ["wheel" "networkmanager"]
-          ++ lib.optional config.hardware.i2c.enable "i2c"
-          ++ lib.optional config.virtualisation.libvirtd.enable "libvirtd";
-      })
-    [user]);
+    users.users.${user} = {
+      isNormalUser = true;
+      extraGroups =
+        ["wheel" "networkmanager"]
+        ++ lib.optional config.hardware.i2c.enable "i2c"
+        ++ lib.optional config.virtualisation.libvirtd.enable "libvirtd";
+    };
   };
 }
