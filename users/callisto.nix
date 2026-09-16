4faeb6bce8b7efb968d4786730f88528d519e4b8
niridium <niridium@tuta.io>
2026-09-16 18:50:05 +0200

    feat: enable eclipse-java for callisto user

--- users/callisto.nix
+++ users/callisto.nix
@@ -4,37 +4,43 @@
     gelly
     tutanota-desktop
   ];
-  programs.rclone = {
-    enable = true;
-    remotes = {
-      storage-box-1.config = {
-        type = "sftp";
-        host = "u651371.your-storagebox.de";
-        user = "u651371";
-        port = 23;
-        shell_type = "unix";
-        md5sum_command = "md5sum";
-        sha1sum_command = "sha1sum";
-        known_hosts_file = "/home/callisto/.ssh/known_hosts";
-      };
-      storage-box-1-crypted = {
-        config = {
-          type = "crypt";
-          remote = "storage-box-1:encrypted";
+  programs = {
+    eclipse = {
+      enable = true;
+      package = pkgs.eclipses.eclipse-java;
+    };
+    rclone = {
+      enable = true;
+      remotes = {
+        storage-box-1.config = {
+          type = "sftp";
+          host = "u651371.your-storagebox.de";
+          user = "u651371";
+          port = 23;
+          shell_type = "unix";
+          md5sum_command = "md5sum";
+          sha1sum_command = "sha1sum";
+          known_hosts_file = "/home/callisto/.ssh/known_hosts";
         };
-        secrets = {
-          password = "/home/callisto/.config/rclone/storage-box-1-crypted.pswd";
+        storage-box-1-crypted = {
+          config = {
+            type = "crypt";
+            remote = "storage-box-1:encrypted";
+          };
+          secrets = {
+            password = "/home/callisto/.config/rclone/storage-box-1-crypted.pswd";
+          };
         };
-      };
-      cronos-box = {
-        config = {
-          type = "webdav";
-          url = "https://webdav.sole-alkaid.ts.net";
-          vendor = "other";
-        };
-        mounts."/" = {
-          enable = true;
-          mountPoint = "/home/callisto/cronos-box";
+        cronos-box = {
+          config = {
+            type = "webdav";
+            url = "https://webdav.sole-alkaid.ts.net";
+            vendor = "other";
+          };
+          mounts."/" = {
+            enable = true;
+            mountPoint = "/home/callisto/cronos-box";
+          };
         };
       };
     };
